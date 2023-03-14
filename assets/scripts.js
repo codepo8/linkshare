626462d0b481be697fe158f68f30708a0505992f
Christian Heilmann <chris.heilmann@gmail.com>
2023-03-14 09:33:04 +0100

    copy error

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,7 +13,7 @@ document.querySelector('#snippets')?.addEventListener('click', e => {
     let y = e.target.getBoundingClientRect().bottom;    
     document.documentElement.style.setProperty('--mouse-y', y);
     if (e.target.tagName === 'BUTTON') {
-        let node = e.target.parentNode.parentNode;
+        let node = e.target.parentNode;
         let html = document.querySelector('template').innerHTML;
         let bits = node.innerHTML.match(/<a href="([^"]*)">([^<]*)<\/a>([^<]*)<but/);
         html = html.replace('$LINKTEXT',bits[2]);
